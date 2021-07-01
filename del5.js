3bf9e58d3fb4fffa6444482f2fc02060b430c7cb
Amalia <74189129+chocolateprincess666@users.noreply.github.com>
2021-07-01 10:53:07 +0200

    1-3, 5 finished

--- del5.js
+++ del5.js
@@ -3,44 +3,26 @@ const myButton = document.getElementById('button');
 const myUl = document.getElementById('ul');
 const error = document.getElementById('error');
 
-
-
 myButton.addEventListener("click", function() {
-/*
-    if (myInput) {
 
+    const stringValue = myInput.value;
+    const stringLength = stringValue.length;
+
+
+    if (stringLength === 0) {
+        alert("Field must not be empty");
+    
+    }
+    else if (stringLength) {
         const myLi = document.createElement('li');
+        myLi.style.cursor = "pointer";
 
         myUl.appendChild(myLi);
 
-        myLi.innerText = myInput.value;
+        myLi.innerText = stringValue;
+
+        myLi.addEventListener("click", function() {
+            myLi.remove();
+        });
     }
-    else if (!myInput) {
-        error.innerText = "Field must not be empty";
-    }
-*/
-
-const stringValue = myInput.value;
-const stringLength = stringValue.length;
-
-
-if (stringLength === 0) {
-    //error.innerHTML = "Field must not be empty";
-
-    alert("Field must not be empty");
-   
-}
-else if (stringLength) {
-    const myLi = document.createElement('li');
-    myLi.style.cursor = "pointer";
-
-    myUl.appendChild(myLi);
-
-    myLi.innerText = stringValue;
-}
-
-myLi.addEventListener("click", function() {
-    myLi.remove();
-});
-
 });
